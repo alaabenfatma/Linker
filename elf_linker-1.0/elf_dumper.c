28b3043908de86b9d7142a9a000c74d05b95d39d
Wassim Ayari <wassimayari10@gmail.com>
2019-12-13 11:35:45 +0100

    added elf.h header file and modified structure in elf_dumper

--- elf_linker-1.0/elf_dumper.c
+++ elf_linker-1.0/elf_dumper.c
@@ -3,29 +3,20 @@
 #include <stdio.h>
 
 #include <stdlib.h>
-
-typedef struct{
-    uint8_t ID[16];
-    uint16_t File_Type;
-    uint16_t Machine_Cible;
-    uint32_t Version;
-}HEAD;
+#include <elf.h>
 
 //local header
-HEAD header;
+Elf64_Ehdr header;
 void read_magic(FILE *file){
   //Il faut essayer avec le fichier foo.o
 
   if (file) {
     fread( & header, 1, sizeof(header), file);
-    if (header.ID[0] == 0x7f && header.ID[1] == 'E' && header.ID[2] == 'L' && header.ID[3] == 'F') {
+    if (header.e_ident[0] == 0x7f && header.e_ident[1] == 'E' && header.e_ident[2] == 'L' && header.e_ident[3] == 'F') {
       printf("Magique: ");
       for (int i = 0; i < 16; i++) {
-        printf("%02x ", header.ID[i]);
+        printf("%02x ", header.e_ident[i]);
       }
-        printf("%02x ", header.File_Type);
-        printf("%02x ", header.Machine_Cible);
-        printf("%02x ", header.Version);
       printf("\n");
     } else {
       printf("Not a valid ELF format.\n");
@@ -38,37 +29,37 @@ void read_magic(FILE *file){
   }
 }
 void get_class(){
-    if(header.ID[4]==0x0){
+    if(header.e_ident[4]==0x0){
         printf("Classe:\tN'est pas valable.\n");
     }
-    else if(header.ID[4]==0x1){
+    else if(header.e_ident[4]==0x1){
         printf("Classe:\tELF32\n");
     }
-    else if(header.ID[4]==0x2){
+    else if(header.e_ident[4]==0x2){
         printf("Classe:\tELF64\n");
     }
 }
 void get_donnees(){
-    if(header.ID[5]==0x0){
+    if(header.e_ident[5]==0x0){
         printf("Données: Format de données inconnu.\n");
     }
-    else if(header.ID[5]==0x1){
+    else if(header.e_ident[5]==0x1){
         printf("Données: Complément à deux, petit boutiste.\n");
     }
-    else if(header.ID[5]==0x2){
+    else if(header.e_ident[5]==0x2){
         printf("Données: Complément à deux, gros boutiste.\n");
     }
 }
-void get_version(){
-    if(header.ID[6]==0x0){
-        printf("Version: 0 (Version non valable)\n");
+void get_e_version(){
+    if(header.e_ident[6]==0x0){
+        printf("e_version: 0 (e_version non valable)\n");
     }
-    else if(header.ID[6]==0x1){
-        printf("Version: 1 (current)\n");
+    else if(header.e_ident[6]==0x1){
+        printf("e_version: 1 (current)\n");
     }
 }
 void get_os(){
-    switch(header.ID[7]){
+    switch(header.e_ident[7]){
         case 0x0:
             printf("OS/ABI: UNIX System V");
             break;
@@ -105,11 +96,11 @@ void get_os(){
     printf("\n");
 }
 void get_ABI(){
-    printf("Version ABI: %x\n",header.ID[8]);
+    printf("e_version ABI: %x\n",header.e_ident[8]);
 }
 void type_fichier(){
     printf("Type: ");
-    switch(header.File_Type){
+    switch(header.e_type){
         case 0x0: printf("Type inconnu"); break;
         case 0x1: printf("REL"); break;
         case 0x2: printf("EXEC"); break;
@@ -121,7 +112,7 @@ void type_fichier(){
 
 void get_machine(){
         printf("Machine Cible: ");
-        switch(header.Machine_Cible){
+        switch(header.e_machine){
                 case 0x0: printf("Aucune\n");break;
                 case 0x2: printf("SPARC\n");break;
                 case 0x3: printf("Intel 80386\n");break;
@@ -138,8 +129,8 @@ void get_machine(){
         }
 }
 
-void get_version2(){
-    printf("Version: 0x%x \n", header.Version);
+void get_e_version2(){
+    printf("e_version: 0x%x \n", header.e_version);
 }
 
 int main(int argc, char * argv[]) {
@@ -147,11 +138,11 @@ int main(int argc, char * argv[]) {
     read_magic(file);
     get_class();
     get_donnees();
-    get_version();
+    get_e_version();
     get_os();
     get_ABI();
     type_fichier();
     get_machine();
-    get_version2();
+    get_e_version2();
   return 0;
 }
