452cccfdc1de5bbb0a9b2a3f993128e1e7e2f1b8
johanDa9u <57444001+johanDa9u@users.noreply.github.com>
2020-01-08 18:17:02 +0100

    update of brain.h

--- elf_linker-1.0/brain.h
+++ elf_linker-1.0/brain.h
@@ -20,6 +20,7 @@
 #define bswap_64(x) ((unsigned int)__builtin_bswap64(x))
 
 //Etape 1 interfaces
+char* get_func_name(FILE* elfFile, Elf32_Ehdr header, int i_table_chaine, int i_nom);
 void dump_sections(FILE *file, int i);
 void etape1(FILE *f);
 void etape2(FILE *f);
